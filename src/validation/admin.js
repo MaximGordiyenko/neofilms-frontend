20ea0ac37e06cf3107c6808cc11b2471ca1d04f8
MaximGordiyenko <maxim.gordiyenko@gmail.com>
2024-05-15 21:27:57 +0300

    fix password checking

--- src/validation/admin.js
+++ src/validation/admin.js
@@ -12,6 +12,7 @@ export const loginSchema = Yup.object({
     .min(8, 'Password must be at least 8 characters long')
     .matches(/[a-z]+/, 'Password must contain at least one lowercase letter')
     .matches(/[A-Z]+/, 'Password must contain at least one uppercase letter')
-    .matches(/[@$!%*?&]+/, 'Password must contain at least one special character')
+    .matches(/[0-9]+/, 'Password must contain at least one digit')
+    .matches(/[@$!%\\*\\?&]+/, 'Password must contain at least one special character')
     .required('Password is required')
 });
