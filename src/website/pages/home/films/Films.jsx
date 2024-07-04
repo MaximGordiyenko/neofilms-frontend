677d7d9b7a4f4b6fb68a512d56b8c9f6186e6aae
MaximGordiyenko <maxim.gordiyenko@gmail.com>
2024-07-04 17:29:55 +0300

    fix: upd

--- src/website/pages/home/films/Films.jsx
+++ src/website/pages/home/films/Films.jsx
@@ -5,41 +5,41 @@ import { FILM_CARDS } from '../../../constants/filmsConstants';
 import FilmCards from './FilmsCards';
 import { Button } from '../../../components/button/Button';
 import { useNavigate } from 'react-router-dom';
-import {useEffect, useState} from "react";
-import {getMovies} from "../../../../api/movie";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import {HomeFilmCards} from "./HomeFilmCards";
+import { HomeFilmCards } from "./HomeFilmCards";
+import Spinner from "../../../components/loader/Spinner";
 
 export const Films = () => {
-    const [films, setFilms] = useState(null);
-    const [loading, setLoading] = useState(true);
+  const [films, setFilms] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const fetchMovies = async () => {
-            try {
-                setLoading(true);
-                const response = await axios.get('http://57.151.104.191:8888/api/pages/movies');
-                const movies = response.data;
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get('http://57.151.104.191:8888/api/pages/movies');
+        const movies = response.data;
 
-                const moviesWithPosters = await Promise.all(movies.map(async (movie) => {
-                    const posterResponse = await axios.get(`http://57.151.104.191:8888/api/pages/movie/${movie.id}/poster`, { responseType: 'blob' });
-                    const posterUrl = URL.createObjectURL(posterResponse.data);
-                    return { ...movie, posterUrl };
-                }));
+        const moviesWithPosters = await Promise.all(movies.map(async (movie) => {
+          const posterResponse = await axios.get(`http://57.151.104.191:8888/api/pages/movie/${movie.id}/poster`, { responseType: 'blob' });
+          const posterUrl = URL.createObjectURL(posterResponse.data);
+          return { ...movie, posterUrl };
+        }));
 
-                setFilms(moviesWithPosters);
-                console.error('response', response);
-            } catch (error) {
-                console.error('Error fetching movies:', error);
-            } finally {
-                setLoading(false);
-            }
-        };
+        setFilms(moviesWithPosters);
+        console.error('response', response);
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-        fetchMovies();
-    }, []);
+    fetchMovies();
+  }, []);
 
-    console.log(films, 'get films')
+  console.log(films, 'get films');
 
   const navigate = useNavigate();
   const handleNav = () => {
@@ -59,26 +59,25 @@ export const Films = () => {
         <img src={fAward} alt={'fAward-films'} />
         <img src={sAward} alt={'sAward-films'} />
       </div>
-        {
-            loading ?
-                <p>Loading...</p>
-                :
-                <div className={'film-cards-box'}>
-                    {films && films.slice(-3).map((film) => (
-                        <HomeFilmCards
-                            key={film.id}
-                            img={film.posterUrl}
-                            title={film.title}
-                            date={new Date(film.release_date).toLocaleDateString()}
-                            description={film.description}
-                            movieLink={film.movie_link}
-                            path={film.id}
-                            id={film.id}
-                        />
-                    ))}
-                </div>
-        }
-        <Button onClick={handleNav} text={'explore full library'} style={{width: '100%'}}/>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className={'film-cards-box'}>
+          {films && films.slice(-3).map((film) => (
+            <HomeFilmCards
+              key={film.id}
+              img={film.posterUrl}
+              title={film.title}
+              date={new Date(film.release_date).toLocaleDateString()}
+              description={film.description}
+              movieLink={film.movie_link}
+              path={film.id}
+              id={film.id}
+            />
+          ))}
+        </div>
+      )}
+      <Button onClick={handleNav} text={'explore full library'} style={{width: '100%'}} />
     </div>
   );
 };
--- src/website/pages/web3/neoNFT/body/cards/NeoNftCards.jsx
+++ src/website/pages/web3/neoNFT/body/cards/NeoNftCards.jsx
@@ -1,37 +1,14 @@
 import './style.scss';
 import { NFT_CARDS } from '../../../../../constants/web3';
 import ProgressBar from '../../../../../components/progressbar/Progressbar';
-import {useEffect, useState} from "react";
-import axios from "axios";
-import {getProjects} from "../../../../../../api/project";
-import {getImage} from "../../../../../../api/project";
+import { useEffect, useState } from "react";
+import { getProjects, getImage } from "../../../../../../api/project";
+import Spinner from "../../../../../components/loader/Spinner";
 
 export const NeoNftCards = () => {
-  const [projectsData,setProjectsData ] = useState([]);
+  const [projectsData, setProjectsData] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  // useEffect(() => {
-  //   const fetchProjects = async () => {
-  //     try {
-  //       setLoading(true);
-  //       const response = await axios.get('http://57.151.104.191:8888/api/pages/projects');
-  //       const projectsData = response.data;
-  //
-  //       const cardsWithImages = await Promise.all(projectsData.map(async (project) => {
-  //         const imageResponse = await axios.get(`http://57.151.104.191:8888/api/pages/projects/${project.id}/image`, { responseType: 'blob' });
-  //         const imageUrl = URL.createObjectURL(imageResponse.data);
-  //         return { ...project, imageUrl };
-  //       }));
-  //       setProjectsData(cardsWithImages);
-  //     } catch (error) {
-  //       console.error('Error fetching projects:', error);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-  //
-  //   fetchProjects();
-  // }, []);
   useEffect(() => {
     const fetchProjects = async () => {
       try {
@@ -54,27 +31,33 @@ export const NeoNftCards = () => {
 
     fetchProjects();
   }, []);
-  console.log(projectsData, 'projects')
+
+  console.log(projectsData, 'projects');
+
   return (
     <div className={'cards-nft'}>
-      {projectsData.slice(-3).map((item, i) => {
-        console.log(item, 'item')
-        return (
-          <div className={'card-nft'} key={i}>
-            <div className={'card-inner-content'}>
-              <img src={item.imageUrl} className='image-nft-card' />
-              <h4>{item.name}</h4>
-              <p>{item.description}</p>
-              <ProgressBar progress={item.completion} />
+      {loading ? (
+        <Spinner />
+      ) : (
+        projectsData.slice(-3).map((item, i) => {
+          console.log(item, 'item');
+          return (
+            <div className={'card-nft'} key={i}>
+              <div className={'card-inner-content'}>
+                <img src={item.imageUrl} className='image-nft-card' />
+                <h4>{item.name}</h4>
+                <p>{item.description}</p>
+                <ProgressBar progress={item.completion} />
+              </div>
+              <div className={'pg-bar-points'}>
+                <span>pre-prod</span>
+                <span>production</span>
+                <span>release</span>
+              </div>
             </div>
-            <div className={'pg-bar-points'}>
-              <span>pre-prod</span>
-              <span>production</span>
-              <span>release</span>
-            </div>
-          </div>
-        );
-      })}
+          );
+        })
+      )}
     </div>
   );
 };
