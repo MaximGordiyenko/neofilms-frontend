837e24066461e67003d58260b8e45d30c954de5b
MaximGordiyenko <maxim.gordiyenko@gmail.com>
2024-07-08 12:16:37 +0300

    Implement API for live streaming

--- src/website/pages/live/Live.jsx
+++ src/website/pages/live/Live.jsx
@@ -1,22 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import Header from '../../components/header/Header';
 import './style.scss';
-import clipleft from '../../assets/images/clipleft.svg';
-import { NAVBAR_TABS } from '../../constants/homePageConst';
-import { Link, useLocation } from 'react-router-dom';
-import clipright from '../../assets/images/clipright.svg';
-import { useEffect, useState } from 'react';
 import { FooterCreds } from '../../components/credsFooter/FooterCreds';
+import { MobMenu } from '../../components/mobileMenu/MobMenu';
+import { Navbar } from '../../components/navbar/Navbar';
+import * as liveApi from '../../../api/live';
+import clipleft from '../../assets/images/clipleft.svg';
+import clipright from '../../assets/images/clipright.svg';
 import upperDots from '../../assets/images/image 16.svg';
 import downDots from '../../assets/images/image 18.svg';
 import mobMenu from '../../assets/images/sidebar-menu-mob.svg';
-import { MobMenu } from '../../components/mobileMenu/MobMenu';
-import { Navbar } from '../../components/navbar/Navbar';
 
 export const Live = () => {
   const location = useLocation();
   const [initialActive, setInitialActive] = useState(location.pathname);
   const isMobile = window.innerWidth <= 430;
   const [isMobileMenuOpen, setIsMobMenuOpen] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+  const [liveUrl, setLiveUrl] = useState(null);
 
   const handleOpenMobMenu = () => {
     setIsMobMenuOpen((prev) => !prev);
@@ -25,6 +27,19 @@ export const Live = () => {
   useEffect(() => {
     setInitialActive(location.pathname);
   }, [location.pathname]);
+
+  useEffect(() => {
+    liveApi.status().then((res) => {
+      console.log("live status", res.data.is_active, res.data.access_for_nft);
+      setIsActive(res.data.is_active);
+      liveApi.check().then((res) => {
+        console.log("check", res.data.live_url);
+        setLiveUrl(res.data.live_url);
+      });
+    }).catch((err) => {
+      console.error("Error fetching live status", err);
+    });
+  }, []);
 
   return (
     <div className={'live-wrapper'}>
@@ -35,10 +50,36 @@ export const Live = () => {
           {isMobileMenuOpen && <MobMenu onClose={handleOpenMobMenu} isOpen={isMobileMenuOpen}/>}
           <img src={downDots} alt={'up-dots'} className={'live-down-dots'}/>
         </div>
-        <div className={'live-title-box'}>
-          <h2 className={'live-title'}>Livestream Media Hub</h2>
-          <div className={'live-soon'}>
-            <h4 className={'soon-span'}>coming soon</h4>
+        {
+          isActive ? (
+            liveUrl ? (
+              <div className="live-stream-box">
+                <iframe
+                  width="1501"
+                  height="776"
+                  src={liveUrl}
+                  title="NEO Live Stream"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerpolicy="strict-origin-when-cross-origin"
+                  allowfullscreen>
+                </iframe>
+              </div>
+            ) : (
+              <div className="live-stream-box">
+                <h2 className="live-stream-title">Live stream is currently offline</h2>
+              </div>
+            )
+          ) : (
+            <div className="live-stream-box">
+              <h2 className="live-stream-title">Live stream is currently offline</h2>
+            </div>
+          )
+        }
+        <div className="live-title-box">
+          <h2 className="live-title">Livestream Media Hub</h2>
+          <div className="live-soon">
+            <h4 className="soon-span">coming soon</h4>
           </div>
         </div>
       </div>
