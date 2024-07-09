ebb987fead735e74002afe41f1061391607ee648
MaximGordiyenko <maxim.gordiyenko@gmail.com>
2024-07-09 16:48:25 +0300

    fix api for img url

--- src/website/pages/home/films/Films.jsx
+++ src/website/pages/home/films/Films.jsx
@@ -6,42 +6,21 @@ import FilmCards from './FilmsCards';
 import { Button } from '../../../components/button/Button';
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from "react";
-import axios from "axios";
 import { HomeFilmCards } from "./HomeFilmCards";
 import Spinner from "../../../components/loader/Spinner";
+import { useDispatch, useSelector } from 'react-redux';
+import { getMovies } from '../../../../admin_panel/store/thunk/movie.api';
 
 export const Films = () => {
-  const [films, setFilms] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchMovies = async () => {
-      try {
-        setLoading(true);
-        const response = await axios.get('http://57.151.104.191:8888/api/pages/movies');
-        const movies = response.data;
-
-        const moviesWithPosters = await Promise.all(movies.map(async (movie) => {
-          const posterResponse = await axios.get(`http://57.151.104.191:8888/api/pages/movie/${movie.id}/poster`, { responseType: 'blob' });
-          const posterUrl = URL.createObjectURL(posterResponse.data);
-          return { ...movie, posterUrl };
-        }));
-
-        setFilms(moviesWithPosters);
-        console.error('response', response);
-      } catch (error) {
-        console.error('Error fetching movies:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchMovies();
-  }, []);
-
-  console.log(films, 'get films');
-
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+  
+  useEffect(() => {
+    dispatch(getMovies());
+  }, [dispatch, getMovies]);
+  
+  const { movies, status } = useSelector((state) => state?.movie);
+  
   const handleNav = () => {
     navigate('/all_movies', { replace: true });
     window.scrollTo(0, 0);
@@ -59,14 +38,14 @@ export const Films = () => {
         <img src={fAward} alt={'fAward-films'} />
         <img src={sAward} alt={'sAward-films'} />
       </div>
-      {loading ? (
+      {status === 'loading' ? (
         <Spinner />
       ) : (
         <div className={'film-cards-box'}>
-          {films && films.slice(-3).map((film) => (
+          {movies && movies?.slice(-3)?.map((film) => (
             <HomeFilmCards
               key={film.id}
-              img={film.posterUrl}
+              img={`/api/pages/movie/${film?.id}/poster`}
               title={film.title}
               date={new Date(film.release_date).toLocaleDateString()}
               description={film.description}
