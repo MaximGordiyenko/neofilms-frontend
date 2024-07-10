46a698947a5fd608c1fdca233966398f458f21ff
MaximGordiyenko <maxim.gordiyenko@gmail.com>
2024-07-10 18:17:19 +0300

    fix: upd

--- src/website/pages/filmDetails/FilmDetails.jsx
+++ src/website/pages/filmDetails/FilmDetails.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import {useNavigate, useParams} from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 import Header from '../../components/header/Header';
 import './style.scss';
 import { FooterCreds } from '../../components/credsFooter/FooterCreds';
 import blackStroke from '../../assets/images/footer-hp-placeholder.svg';
 import imdb from '../../assets/images/IMDb.png';
+import {getMovie, getPoster} from "../../../api/movie";
 
 const FilmDetails = () => {
     const { id } = useParams();
@@ -24,17 +25,13 @@ const FilmDetails = () => {
         let isMounted = true;
         const fetchFilmDetails = async () => {
             try {
-                const [filmResponse, posterResponse] = await axios.all([
-                    axios.get(`http://57.151.104.191:8888/api/pages/movie/${id}`),
-                    axios.get(`http://57.151.104.191:8888/api/pages/movie/${id}/poster`, {
-                        responseType: 'blob'
-                    })
+                const [filmResponse, posterUrl] = await Promise.all([
+                    getMovie(id),
+                    getPoster(id)
                 ]);
 
                 if (isMounted) {
                     setFilm(filmResponse.data);
-                    const posterBlob = posterResponse.data;
-                    const posterUrl = URL.createObjectURL(posterBlob);
                     setPoster(posterUrl);
                 }
             } catch (err) {
@@ -53,14 +50,11 @@ const FilmDetails = () => {
         };
     }, [id]);
 
-    useEffect(() => {
-
-    }, []);
-
     const formatDate = (milliseconds) => {
         const date = new Date(milliseconds);
         return date.toLocaleDateString();
     };
+
     const handleNav = () => {
         navigate(`${film.movie_link}}`, { replace: true });
         window.scrollTo(0, 0);
@@ -79,46 +73,46 @@ const FilmDetails = () => {
     }
 
     const { title, starring, release_date, written_by, directed_by } = film;
-    console.log(film, 'film')
+
     return (
-        <div className="movie-details-wrapper">
-            <div className="bg-details-box" style={{ backgroundImage: `url(${poster || film.backgroundImg})` }}>
-                <Header />
-                <div className="title-details-box">
-                    <h2>{title}</h2>
-                    <img src={imdb} alt='imdb' onClick={handleNav}/>
-                </div>
-            </div>
-            <div className="movie-details-desc">
-                <div className="announcement">
-                    <div className="date-release">
-                        <span>release date</span>
-                        <h2>{formatDate(release_date)}</h2>
-                    </div>
-                    <div className="written-by">
-                        <span>written by</span>
-                        <h2>{written_by}</h2>
-                    </div>
-                    <div className="directed-by">
-                        <span>directed by</span>
-                        <h2>{directed_by}</h2>
-                    </div>
-                </div>
-                <div className="desc-text">
-                    {starring && (
-                      <>
-                          <span>starring</span>
-                            {starring.map((actor, index) => (
-                                <h3 key={index}>{actor}</h3>
-                            ))}
-                        </>
-                    )}
-                    <p>{film.description}</p>
-                </div>
-            </div>
-            <img src={blackStroke} alt="black-stroke-details" className="stroke-placeholder-det" />
-            <FooterCreds />
-        </div>
+      <div className="movie-details-wrapper">
+          <div className="bg-details-box" style={{ backgroundImage: `url(${poster || film.backgroundImg})` }}>
+              <Header />
+              <div className="title-details-box">
+                  <h2>{title}</h2>
+                  <img src={imdb} alt='imdb' onClick={handleNav} />
+              </div>
+          </div>
+          <div className="movie-details-desc">
+              <div className="announcement">
+                  <div className="date-release">
+                      <span>release date</span>
+                      <h2>{formatDate(release_date)}</h2>
+                  </div>
+                  <div className="written-by">
+                      <span>written by</span>
+                      <h2>{written_by}</h2>
+                  </div>
+                  <div className="directed-by">
+                      <span>directed by</span>
+                      <h2>{directed_by}</h2>
+                  </div>
+              </div>
+              <div className="desc-text">
+                  {starring && (
+                    <>
+                        <span>starring</span>
+                        {starring.map((actor, index) => (
+                          <h3 key={index}>{actor}</h3>
+                        ))}
+                    </>
+                  )}
+                  <p>{film.description}</p>
+              </div>
+          </div>
+          <img src={blackStroke} alt="black-stroke-details" className="stroke-placeholder-det" />
+          <FooterCreds />
+      </div>
     );
 };
 
