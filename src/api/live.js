8d70ccf4b630837144d7ece55aec1c10db3c43c8
MaximGordiyenko <maxim.gordiyenko@gmail.com>
2024-07-10 04:17:57 +0300

    update api for the live

--- src/api/live.js
+++ src/api/live.js
@@ -1,14 +1,29 @@
 import axios from 'axios';
 
 
+
+export function auth() {
+	return `/api/live/auth`;
+}
+
+export function callback() {
+	return `/api/live/callback`;
+}
+
+export async function status() {
+	return await axios.get(`/api/live/status`, {
+			withCredentials: true
+		});
+}
+
 export async function check() {
 	return await axios.get(`/api/live/check`, {
 			withCredentials: true
 		});
 }
 
-export async function status() {
-	return await axios.get(`/api/live/status`, {
+export async function info() {
+	return await axios.get(`/api/live/info`, {
 			withCredentials: true
 		});
 }
