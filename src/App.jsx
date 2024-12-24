05662ac17d060d0fcf9928b009bd96b689983ad3
MaximGordiyenko <maxim.gordiyenko@gmail.com>
2024-12-24 16:49:57 +0200

    feat: move Shop page to maintenance and update gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { Route, Routes, useNavigate, useLocation } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 
 import './App.scss';
 
-import React, { lazy, Suspense, useState, useEffect } from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import { ROUTE } from './constants.js';
-import { createTheme, ThemeProvider, responsiveFontSizes } from "@mui/material/styles";
+import { createTheme, ThemeProvider, responsiveFontSizes } from '@mui/material/styles';
 import { Layout } from './admin_panel/components/layouts/Layout.jsx';
 import { NavigationTabs } from './admin_panel/components/tabs/NavigationTabs.jsx';
 import { LoginPage } from './admin_panel/pages/sign-in/LoginPage.jsx';
@@ -19,7 +19,7 @@ import { CalendarPage } from './admin_panel/pages/calendar/CalendarPage.jsx';
 import { CreateCalendarPage } from './admin_panel/pages/calendar/CreateCalendarPage.jsx';
 import { CalendarEditPage } from './admin_panel/pages/calendar/CalendarEditPage.jsx';
 import { NoMatch } from './NoMatch.jsx';
-import { light, dark } from './theme-config.js';
+import { light } from './theme-config.js';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import OnlyAdmin from './utils/OnlyAdmin';
@@ -27,11 +27,12 @@ import { CreateCastingPage } from './admin_panel/pages/casting/CreateCastingPage
 import { CastingEditPage } from './admin_panel/pages/casting/CastingEditPage';
 import { CastingPage } from './admin_panel/pages/casting/CastingPage';
 import { adminLogout } from './admin_panel/store/thunk/admin.api';
-import Shop from "./website/pages/shop/Shop";
+// import Shop from './website/pages/shop/Shop';
 import { LatestNewsPage } from './admin_panel/pages/latest-news/LatestNewsPage';
 import { NewsEditPage } from './admin_panel/pages/latest-news/NewsEditPage';
 import { SettingsPage } from './admin_panel/pages/settings/SettingsPage';
-import loader from './website/assets/images/Preloader.gif'
+import loader from './website/assets/images/Preloader.gif';
+import { UnderConstruction } from './website/pages/underConstruction/SoonPage';
 
 const HomePage = lazy(() => import('./website/pages/home/HomePage'));
 const Live = lazy(() => import('./website/pages/live/Live'));
@@ -63,147 +64,167 @@ export const App = () => {
 
   const logout = async () => {
     await dispatch(adminLogout());
-    navigate(`/${ROUTE.admin}/${ROUTE.login}`, { replace: true })
-  }
+    navigate(`/${ROUTE.admin}/${ROUTE.login}`, { replace: true });
+  };
 
   return (
     <ThemeProvider theme={responsiveFontSizes(themeLight)}>
-      <Suspense fallback={<div className="holder-app"><img src={loader} className="loader-gif-app"/></div>}>
+      <Suspense
+        fallback={
+          <div className="holder-app">
+            <img src={loader} className="loader-gif-app" alt='' />
+          </div>
+        }>
         <Routes>
           {/* user frontend routes */}
-          <Route path={'/'} element={<HomePage/>}/>
-          <Route path={'/live'} element={<Live/>}/>
-          <Route path={'/about'} element={<AboutPage/>}/>
-          <Route path={'/all_movies'} element={<AllMovies/>}/>
-          <Route path={'/news'} element={<News/>}/>
-          <Route path={'/product'} element={<Shop/>}/>
-          <Route path={'/services'} element={<Services/>}/>
+          <Route path={'/'} element={<HomePage />} />
+          <Route path={'/live'} element={<Live />} />
+          <Route path={'/about'} element={<AboutPage />} />
+          <Route path={'/all_movies'} element={<AllMovies />} />
+          <Route path={'/news'} element={<News />} />
+          {/*<Route path={'/product'} element={<Shop/>}/>*/}
+          <Route path={'/product'} element={<UnderConstruction />} />
+          <Route path={'/services'} element={<Services />} />
           {/*<Route path={'/web3'} element={<UnderConstruction />} />*/}
-          <Route path={'/web3/neo-nft'} element={<NeoNft/>}/>
-          <Route path={'/web3/stake'} element={<NeoStaking/>}/>
-          <Route path={'/casting'} element={<Casting/>}/>
-          <Route path={'/web3/redeem'} element={<Redeem/>}/>
-          <Route path={'/contacts'} element={<Contact/>}/>
-          <Route path={'/film-details/:id'} element={<FilmDetails/>}/>
+          <Route path={'/web3/neo-nft'} element={<NeoNft />} />
+          <Route path={'/web3/stake'} element={<NeoStaking />} />
+          <Route path={'/casting'} element={<Casting />} />
+          <Route path={'/web3/redeem'} element={<Redeem />} />
+          <Route path={'/contacts'} element={<Contact />} />
+          <Route path={'/film-details/:id'} element={<FilmDetails />} />
           <Route path={'/cast-film-details/:casting_id'} element={<CastFilmPage />} />
           {/* admin panel routes */}
-          <Route path={`/${ROUTE.admin}/${ROUTE.login}`} element={<LoginPage/>}/>
-          <Route element={<Layout logout={logout}/>}>
+          <Route path={`/${ROUTE.admin}/${ROUTE.login}`} element={<LoginPage />} />
+          <Route element={<Layout logout={logout} />}>
             <Route
               path={ROUTE.admin}
               element={
-                <NavigationTabs
-                  tab={tab}
-                  onChangeTab={(event, newValue) => setTab(newValue)}
-                />
+                <NavigationTabs tab={tab} onChangeTab={(event, newValue) => setTab(newValue)} />
               }>
+              <Route index element={<LatestNewsPage />} />
+              <Route
+                path={ROUTE.latestNews}
+                element={<OnlyAdmin element={<LatestNewsPage tab={tab} cards={news} />} />}
+              />
 
-              <Route index element={<LatestNewsPage/>}/>
-              <Route path={ROUTE.latestNews} element={<OnlyAdmin element={
-                <LatestNewsPage
-                  tab={tab}
-                  cards={news}
-                />
-              }/>
-              }/>
+              <Route index element={<MoviesPage />} />
+              <Route
+                path={ROUTE.allMovies}
+                element={
+                  <OnlyAdmin
+                    element={
+                      <MoviesPage
+                        tab={tab}
+                        cards={movies}
+                        onAdd={() =>
+                          navigate(`/${ROUTE.admin}/${ROUTE.allMovies}/${ROUTE.createMovie}`)
+                        }
+                        buttonName="Add Movie"
+                      />
+                    }
+                  />
+                }
+              />
 
-              <Route index element={<MoviesPage/>}/>
-              <Route path={ROUTE.allMovies} element={<OnlyAdmin element={
-                <MoviesPage
-                  tab={tab}
-                  cards={movies}
-                  onAdd={() => navigate(`/${ROUTE.admin}/${ROUTE.allMovies}/${ROUTE.createMovie}`)}
-                  buttonName="Add Movie"
-                />
-              }/>
-              }/>
+              <Route index element={<Web3ProjectPage />} />
+              <Route
+                path={ROUTE.web3project}
+                element={
+                  <OnlyAdmin
+                    element={
+                      <Web3ProjectPage
+                        tab={tab}
+                        cards={projects}
+                        onAdd={() =>
+                          navigate(`/${ROUTE.admin}/${ROUTE.web3project}/${ROUTE.createProject}`)
+                        }
+                        buttonName="Add Project"
+                      />
+                    }
+                  />
+                }
+              />
 
-              <Route index element={<Web3ProjectPage/>}/>
-              <Route path={ROUTE.web3project} element={<OnlyAdmin element={
-                <Web3ProjectPage
-                  tab={tab}
-                  cards={projects}
-                  onAdd={() => navigate(`/${ROUTE.admin}/${ROUTE.web3project}/${ROUTE.createProject}`)}
-                  buttonName="Add Project"
-                />
-              }/>
-              }/>
+              <Route index element={<CalendarPage />} />
+              <Route
+                path={ROUTE.calendar}
+                element={
+                  <OnlyAdmin
+                    element={
+                      <CalendarPage
+                        tab={tab}
+                        cards={calendars}
+                        onAdd={() =>
+                          navigate(`/${ROUTE.admin}/${ROUTE.calendar}/${ROUTE.createCalendar}`)
+                        }
+                        buttonName="Add Event"
+                      />
+                    }
+                  />
+                }
+              />
 
-              <Route index element={<CalendarPage/>}/>
-              <Route path={ROUTE.calendar} element={<OnlyAdmin element={
-                <CalendarPage
-                  tab={tab}
-                  cards={calendars}
-                  onAdd={() => navigate(`/${ROUTE.admin}/${ROUTE.calendar}/${ROUTE.createCalendar}`)}
-                  buttonName="Add Event"
-                />
-              }/>
-              }/>
-
-              <Route index element={<CastingPage/>}/>
+              <Route index element={<CastingPage />} />
               <Route
                 path={ROUTE.casting}
                 element={
                   <CastingPage
                     tab={tab}
                     cards={castings}
-                    onAdd={() => navigate(`/${ROUTE.admin}/${ROUTE.casting}/${ROUTE.createCasting}`)}
+                    onAdd={() =>
+                      navigate(`/${ROUTE.admin}/${ROUTE.casting}/${ROUTE.createCasting}`)
+                    }
                     buttonName="Add Casting"
                   />
                 }
               />
 
-              <Route index element={<SettingsPage/>}/>
-              <Route
-                path={ROUTE.settings}
-                element={
-                  <SettingsPage tab={tab}/>
-                }
-              />
+              <Route index element={<SettingsPage />} />
+              <Route path={ROUTE.settings} element={<SettingsPage tab={tab} />} />
             </Route>
 
-            <Route path={`${ROUTE.admin}/${ROUTE.latestNews}/:newsId`}
-                   element={<OnlyAdmin
-                     element={<NewsEditPage/>}/>}
+            <Route
+              path={`${ROUTE.admin}/${ROUTE.latestNews}/:newsId`}
+              element={<OnlyAdmin element={<NewsEditPage />} />}
             />
 
-            <Route path={`${ROUTE.admin}/${ROUTE.allMovies}/${ROUTE.createMovie}`}
-                   element={<OnlyAdmin
-                     element={<CreateMoviePage/>}/>}
+            <Route
+              path={`${ROUTE.admin}/${ROUTE.allMovies}/${ROUTE.createMovie}`}
+              element={<OnlyAdmin element={<CreateMoviePage />} />}
             />
-            <Route path={`${ROUTE.admin}/${ROUTE.allMovies}/:movieId`}
-                   element={<OnlyAdmin
-                     element={<MovieEditPage/>}/>}
-            />
-
-            <Route path={`${ROUTE.admin}/${ROUTE.web3project}/${ROUTE.createProject}`}
-                   element={<OnlyAdmin
-                     element={<CreateProjectPage/>}/>}
-            />
-            <Route path={`${ROUTE.admin}/${ROUTE.web3project}/:projectId`}
-                   element={<OnlyAdmin
-                     element={<ProjectEditPage/>}/>}
+            <Route
+              path={`${ROUTE.admin}/${ROUTE.allMovies}/:movieId`}
+              element={<OnlyAdmin element={<MovieEditPage />} />}
             />
 
-            <Route path={`${ROUTE.admin}/${ROUTE.calendar}/${ROUTE.createCalendar}`}
-                   element={<OnlyAdmin
-                     element={<CreateCalendarPage/>}/>}
+            <Route
+              path={`${ROUTE.admin}/${ROUTE.web3project}/${ROUTE.createProject}`}
+              element={<OnlyAdmin element={<CreateProjectPage />} />}
             />
-            <Route path={`${ROUTE.admin}/${ROUTE.calendar}/:calendarId`}
-                   element={<OnlyAdmin
-                     element={<CalendarEditPage/>}/>}
+            <Route
+              path={`${ROUTE.admin}/${ROUTE.web3project}/:projectId`}
+              element={<OnlyAdmin element={<ProjectEditPage />} />}
             />
 
-            <Route path={`${ROUTE.admin}/${ROUTE.casting}/${ROUTE.createCasting}`}
-                   element={<OnlyAdmin
-                     element={<CreateCastingPage/>}/>}
+            <Route
+              path={`${ROUTE.admin}/${ROUTE.calendar}/${ROUTE.createCalendar}`}
+              element={<OnlyAdmin element={<CreateCalendarPage />} />}
             />
-            <Route path={`${ROUTE.admin}/${ROUTE.casting}/:castingId`}
-                   element={<OnlyAdmin
-                     element={<CastingEditPage/>}/>}
+            <Route
+              path={`${ROUTE.admin}/${ROUTE.calendar}/:calendarId`}
+              element={<OnlyAdmin element={<CalendarEditPage />} />}
+            />
+
+            <Route
+              path={`${ROUTE.admin}/${ROUTE.casting}/${ROUTE.createCasting}`}
+              element={<OnlyAdmin element={<CreateCastingPage />} />}
+            />
+            <Route
+              path={`${ROUTE.admin}/${ROUTE.casting}/:castingId`}
+              element={<OnlyAdmin element={<CastingEditPage />} />}
             />
           </Route>
-          <Route path="*" element={<NoMatch/>}/>
+          <Route path="*" element={<NoMatch />} />
         </Routes>
       </Suspense>
       <ToastContainer
---
+++ src/website/pages/underConstruction/SoonPage.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect, useRef } from 'react';
+import Header from '../../components/header/Header';
+import { FooterCreds } from '../../components/credsFooter/FooterCreds';
+import satelite from '../../assets/images/constructor-img.svg';
+import './style.scss';
+import { MobMenu } from '../../components/mobileMenu/MobMenu';
+import menuMobile from '../../assets/images/burger-menu.svg';
+import { Navbar } from '../../components/navbar/Navbar';
+import { Text } from '../../components/text/Text';
+import { ShopifyProduct } from '../../components/product/product';
+
+export const UnderConstruction = () => {
+  const [isMobileMenuOpen, setIsMobMenuOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const isMobile = window.innerWidth <= 430;
+  const navbarRef = useRef(null);
+
+  const handleOpenMobMenu = () => {
+    setIsMobMenuOpen((prev) => !prev);
+  };
+
+  useEffect(() => {
+    // Simulate a loading delay
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // Adjust the timeout duration as needed
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    const navbar = navbarRef.current;
+    if (navbar) {
+      if (isLoading) {
+        navbar.style.bottom = '-10vh'; // Example style
+      } else {
+        navbar.style.bottom = '-10vh'; // Reset style
+      }
+    }
+  }, [isLoading]);
+
+  return (
+    <div className={'soon-page-wrapper'}>
+      <Header />
+      <div className={'soon-page-inner-box'}>
+        <div className={'soon-title'}>
+          <h2 className="soon-title-text">
+            This page <br /> is under construction
+          </h2>
+          <img src={satelite} alt={'under-construction'} className={'under-construction'} />
+        </div>
+        {isMobile ? (
+          <div className={'menu-mob-wrapper'}>
+            <img
+              src={menuMobile}
+              className={'sidebar-mob-btn'}
+              onClick={handleOpenMobMenu}
+              alt={'menuMob-hp'}
+            />
+          </div>
+        ) : (
+          <Navbar ref={navbarRef} />
+        )}
+        {isMobileMenuOpen && <MobMenu onClose={handleOpenMobMenu} isOpen={isMobileMenuOpen} />}
+      </div>
+      <FooterCreds />
+    </div>
+  );
+};
